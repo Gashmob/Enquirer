3ebd56c896ea4afda501539a296cb679432dea55
Kevin Traini <kevin@ktraini.com>
2022-11-11 17:42:59 +0100

    Initial commit

---
+++ include/enquirer/enquirer.hpp
@@ -0,0 +1,10 @@
+/**
+ *     === Enquirer ===
+ * Created by Kevin Traini
+ *      Under GPL-3.0
+ * -----------------------
+ */
+#ifndef ENQUIRER_HPP
+#define ENQUIRER_HPP
+
+#endif //ENQUIRER_HPP
