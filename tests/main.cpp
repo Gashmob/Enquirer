f81ef709c8da53f39f2e945ce8fa8bdd4ecb0b53
Kevin Traini <kevin@ktraini.com>
2022-11-12 13:07:57 +0100

    Add a while loop with confirm in main test

--- tests/main.cpp
+++ tests/main.cpp
@@ -12,26 +12,28 @@ using namespace std;
 int main() {
     cout << "Test Enquirer included version " << ENQUIRER_VERSION << endl;
 
-    string choice = enquirer::select("Choose a function to test:", {
-            "input",
-            "invisible",
-            "select",
-            "confirm"
-    });
+    do {
+        string choice = enquirer::select("Choose a function to test:", {
+                "input",
+                "invisible",
+                "select",
+                "confirm"
+        });
 
-    if (choice == "input") {
-        string answer = enquirer::input("What is you name?", "John Doe");
-        cout << "'" << answer << "'" << endl;
-    } else if (choice == "invisible") {
-        string answer = enquirer::invisible("What is you secret?");
-        cout << "'" << answer << "'" << endl;
-    } else if (choice == "select") {
-        string answer = enquirer::select("What is your favorite color?", {"Red", "Green", "Blue"});
-        cout << "'" << answer << "'" << endl;
-    } else if (choice == "confirm") {
-        bool answer = enquirer::confirm("Do you like this library?", true);
-        cout << "'" << answer << "'" << endl;
-    }
+        if (choice == "input") {
+            string answer = enquirer::input("What is you name?", "John Doe");
+            cout << "'" << answer << "'" << endl;
+        } else if (choice == "invisible") {
+            string answer = enquirer::invisible("What is you secret?");
+            cout << "'" << answer << "'" << endl;
+        } else if (choice == "select") {
+            string answer = enquirer::select("What is your favorite color?", {"Red", "Green", "Blue"});
+            cout << "'" << answer << "'" << endl;
+        } else if (choice == "confirm") {
+            bool answer = enquirer::confirm("Do you like this library?", true);
+            cout << "'" << answer << "'" << endl;
+        }
+    } while (enquirer::confirm("Do you want to test another function?", true));
 
     return 0;
 }
