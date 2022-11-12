6f5c699aad5fc2c5b6494dcdf1241a34ff8a99b0
Kevin Traini <kevin@ktraini.com>
2022-11-12 10:04:42 +0100

    Mv CMakeLists + add main for tests + rm generation of .a and .so

---
+++ tests/main.cpp
@@ -0,0 +1,16 @@
+/**
+ *     === Enquirer ===
+ * Created by Kevin Traini
+ *      Under GPL-3.0
+ * -----------------------
+ */
+#include <iostream>
+#include "enquirer.hpp"
+
+using namespace std;
+
+int main() {
+    cout << "Test Enquirer included version " << ENQUIRER_VERSION << endl;
+
+    return 0;
+}
