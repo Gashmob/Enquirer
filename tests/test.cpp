2bf8b73b0e32043c3977218fd42788e92323cfc4
Kevin Traini <kevin@ktraini.com>
2022-11-17 20:27:02 +0100

    Add first tests for input

--- tests/test.cpp
+++ tests/test.cpp
@@ -9,9 +9,48 @@
 #include "test.hpp"
 
 using namespace std;
+using namespace test;
+
+void construct_test(Test &test) {
+    test.describe("Input", []() {
+        it_pass_fail("return the input", []() {
+            stringstream ss;
+            streambuf *old = cin.rdbuf(ss.rdbuf());
+
+            ss << "Hello world!" << endl;
+            string res = enquirer::input("Type something");
+
+            cin.rdbuf(old);
+
+            assert_equal(res, "Hello world!")
+
+            return PASS;
+        })
+
+        it_pass_fail("complete default value", []() {
+            stringstream ss;
+            streambuf *old = cin.rdbuf(ss.rdbuf());
+
+            ss << "J" << '\t' << endl;
+            string res = enquirer::input("Type something", "John");
+
+            cin.rdbuf(old);
+
+            assert_equal(res, "John")
+
+            return PASS;
+        })
+
+        return PASS;
+    });
+}
 
 int main() {
-    cout << "Enquirer test, version " << ENQUIRER_VERSION << endl;
+    cout << "Enquirer test, version " << ENQUIRER_VERSION << endl << endl;
 
-    return run_tests() ? 0 : 1;
+    Test test;
+
+    construct_test(test);
+
+    return test.run() ? 0 : 1;
 }
