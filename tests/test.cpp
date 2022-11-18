e01a5202f54a81fce867d716017f3654e9f80ed6
Kevin Traini <kevin@ktraini.com>
2022-11-18 16:29:50 +0100

    Add tests for auth

--- tests/test.cpp
+++ tests/test.cpp
@@ -11,9 +11,82 @@
 using namespace std;
 using namespace test;
 
+namespace utils_char {
+    const string arrow_up = "\e[A";
+    const string arrow_down = "\e[B";
+    const string arrow_right = "\e[C";
+    const string arrow_left = "\e[D";
+
+    const string tab = "\t";
+}
+
 void construct_test(Test &test) {
-    test.describe("Auth", []() {
-        return SKIP;
+    test.describe("Auth 1", []() {
+        it_pass_fail("return username and password", []() {
+            stringstream ss;
+            streambuf *old = cin.rdbuf(ss.rdbuf());
+
+            ss << "username" << utils_char::arrow_down << "password" << endl;
+            auto result = enquirer::auth();
+
+            cin.rdbuf(old);
+
+            assert_equal("username", result.first);
+            assert_equal("password", result.second);
+
+            return PASS;
+        })
+
+        it_pass_skip("type password then username", []() {
+            stringstream ss;
+            streambuf *old = cin.rdbuf(ss.rdbuf());
+
+            ss << utils_char::arrow_up << "password" << utils_char::arrow_up << "username" << endl;
+            auto result = enquirer::auth();
+
+            cin.rdbuf(old);
+
+            assert_equal("username", result.first);
+            assert_equal("password", result.second);
+
+            return PASS;
+        })
+
+        return PASS;
+    });
+
+    test.describe("Auth 2", []() {
+        it_pass_fail("correct credentials", []() {
+            stringstream ss;
+            streambuf *old = cin.rdbuf(ss.rdbuf());
+
+            ss << "admin" << utils_char::arrow_down << "admin" << endl;
+            bool result = enquirer::auth([](const pair<string, string> &crd) {
+                return crd.first == "admin" && crd.second == "admin";
+            });
+
+            cin.rdbuf(old);
+
+            assert_true(result);
+
+            return PASS;
+        })
+
+        it_pass_fail("wrong credentials", []() {
+            stringstream ss;
+            streambuf *old = cin.rdbuf(ss.rdbuf());
+
+            ss << "abcde" << utils_char::arrow_down << "abcde" << endl;
+            bool result = enquirer::auth([](const pair<string, string> &crd) {
+                return crd.first == "admin" && crd.second == "admin";
+            });
+
+            cin.rdbuf(old);
+
+            assert_false(result);
+
+            return PASS;
+        })
 
         return PASS;
     });
@@ -55,7 +128,7 @@ void construct_test(Test &test) {
             stringstream ss;
             streambuf *old = cin.rdbuf(ss.rdbuf());
 
-            ss << "J" << '\t' << endl;
+            ss << "J" << utils_char::tab << endl;
             string res = enquirer::input("Type something", "John");
 
             cin.rdbuf(old);
